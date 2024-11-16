b52028613f7bd93126c3e3066bd106f539c7a004
chaCon <montt.octavio@gmail.com>
2024-11-16 11:30:13 +0000

    added error handeling for download and session to download

--- utils/image_downloader.py
+++ utils/image_downloader.py
@@ -1,13 +1,24 @@
+import os
 from http import HTTPStatus
 from pathlib import Path
+from time import sleep
 
 # TODO: REVISAR EL PROBLEMA CON EL IMPORT Y NVIM
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 
 out = Path("{}/assets/hexagrams".format(Path.cwd().parent))
 out.mkdir(parents=True, exist_ok=True)
 base_url = "https://en.m.wikipedia.org/wiki/File:Iching-hexagram-{}.png"
+
+load_dotenv("{}/.env".format(Path.cwd().parent))
+
+headers = {
+    "user-agent": os.getenv("USERAGENT"),
+    "cookie": os.getenv("COOKIE"),
+    "accept": os.getenv("ACCEPT"),
+}
 
 
 def print_report(failed, success):
@@ -27,31 +38,47 @@ def print_report(failed, success):
         print("{}: {}".format(key, value))
 
 
-def main():
+def main(verbose=False):
     """
     This funtion is build to be run from the `utils/` folder
     """
     success_hexagrams = {}
     failed_hexagrams = {}
+
+    session = requests.Session()
+    session.headers.update(headers)
+
     for i in range(1, 65):
         hexagram_url = base_url.format(str(i).zfill(2))
-        r = requests.get(hexagram_url)
+        r = session.get(hexagram_url)
         if r.status_code == HTTPStatus.OK:
             downlaoded = False
             soup = BeautifulSoup(r.text, "html.parser")
             for img_tag in soup.find_all("img"):
                 if "New SVG image" in img_tag.get("alt", []):
+                    download_url = "{}".format(img_tag.get("src"))
+                    out_path = "{}/{}.png".format(out, str(i).zfill(2))
                     try:
-                        download_url = "{}".format(img_tag.get("src"))
-                        out_path = "{}/{}.png".format(out, str(i).zfill(2))
-                        img_r = requests.get(download_url)
-                        with open(out_path, "wb") as f:
-                            f.write(img_r.content)
-
-                        downlaoded = True
+                        img_r = session.get(download_url)
+                        if img_r.status_code == 200:
+                            with open(out_path, "wb") as f:
+                                f.write(img_r.content)
+                            downlaoded = True
+                        else:
+                            failed_hexagrams[i] = "HTTPStatus {}".format(
+                                img_r.status_code
+                            )
                     except Exception as e:
                         print(e)
                         failed_hexagrams[i] = "Download Failed"
+
+                    if verbose:
+                        print(
+                            "Hexagram: {} - downloaded: {} - url: {}".format(
+                                i, downlaoded, download_url
+                            )
+                        )
+                    sleep(0.1)
 
             if downlaoded:
                 success_hexagrams[i] = "Download success"
